e112cd6d72a89b5cb5a5b902a24ef6db226738a1
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-24 22:17:24 +0100

    Add placing ship logic for horizont. & verti. ship

--- src/engine/gameboard.js
+++ src/engine/gameboard.js
@@ -4,12 +4,34 @@ export class Gameboard {
   }
 
   placeShip(ship, row, col) {
-    //Check if provided arguments are in bounds
+    // Check if provided arguments are in bounds
     if (row < 0 || row > 9) {
       throw new Error('The row index is out of bounds');
     }
     if (col < 0 || col > 9) {
       throw new Error('The column index is out of bounds');
+    }
+
+    // Check if horizontal ship isn't out of bounds
+    if (ship.vertical === false) {
+      if (col + ship.length - 1 <= 9) {
+        for (let i = 0; i < ship.length; i++) {
+          this.grid[row][col + i] = ship;
+        }
+      } else {
+        throw new Error('Cannot place ship out of bounds');
+      }
+    }
+
+    // Check if vertical ship isn't out of bounds
+    if (ship.vertical === true) {
+      if (row + ship.length - 1 <= 9) {
+        for (let i = 0; i < ship.length; i++) {
+          this.grid[row + i][col] = ship;
+        }
+      } else {
+        throw new Error('Cannot place ship out of bounds');
+      }
     }
   }
 }
