bf155cc551bdcf654a81cb2641e4aa4111a17f5b
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-25 11:28:58 +0100

    Add helpers: is ships overlap / touch each other

--- src/engine/gameboard.js
+++ src/engine/gameboard.js
@@ -3,6 +3,32 @@ export class Gameboard {
     this.grid = Array.from({ length: 10 }, () => Array(10).fill(null));
   }
 
+  // Helper to check if neighbour cell is null or if even exists (is out of bounds)
+  isEmptyOrNull(row, col) {
+    if (row < 0 || row > 9 || col < 0 || col > 9) return true; // out of bounds = OK, ship can be placed on bound
+    return this.grid[row][col] === null;
+  }
+
+  // Helper to check if position is taken by other ship or if any neighbour cell is taken by other ship
+  canPlaceShip(ship, row, col) {
+    for (let i = 0; i < ship.length; i++) {
+      // Segment position
+      let r = ship.vertical ? row + i : row;
+      let c = ship.vertical ? col : col + i;
+
+      // Check cell and neighbours
+      // dr = delta row, dc = delta cell
+      for (let dr = -1; dr <= 1; dr++) {
+        for (let dc = -1; dc <= 1; dc++) {
+          if (!this.isEmptyOrNull(r + dr, c + dc)) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+
   placeShip(ship, row, col) {
     // Check if provided arguments are in bounds
     if (row < 0 || row > 9) {
@@ -12,9 +38,12 @@ export class Gameboard {
       throw new Error('The column index is out of bounds');
     }
 
-    // Check if horizontal ship isn't out of bounds
+    // Check if horizontal ship isn't out of bounds / overlap other ship / touch other ship
     if (ship.vertical === false) {
       if (col + ship.length - 1 <= 9) {
+        if (!this.canPlaceShip(ship, row, col)) {
+          throw new Error('Ships cannot overlap or touch each other');
+        }
         for (let i = 0; i < ship.length; i++) {
           this.grid[row][col + i] = ship;
         }
@@ -23,9 +52,12 @@ export class Gameboard {
       }
     }
 
-    // Check if vertical ship isn't out of bounds
+    // Check if vertical ship isn't out of bounds / overlap other ship / touch other ship
     if (ship.vertical === true) {
       if (row + ship.length - 1 <= 9) {
+        if (!this.canPlaceShip(ship, row, col)) {
+          throw new Error('Ships cannot overlap or touch each other');
+        }
         for (let i = 0; i < ship.length; i++) {
           this.grid[row + i][col] = ship;
         }
