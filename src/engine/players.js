f92481b891fe81b90d5fce3e3b03cb3cbded87ed
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-26 19:06:27 +0100

    Add helper to get random int wit min & max set

--- src/engine/players.js
+++ src/engine/players.js
@@ -1,5 +1,6 @@
 import { Gameboard } from './gameboard.js';
 import { Ship } from './ship.js';
+import { getRandomIntBetween } from './utils/utils.js';
 
 export class Player {
   constructor() {
@@ -14,14 +15,37 @@ export class Player {
         try {
           this.gameboard.placeShip(
             // Create new ship with randomly set orientation (index 0 or 1 of isVertical array) and coordinates (random from 0-9)
-            new Ship(i, isVertical[Math.round(Math.random())]),
-            Math.floor(Math.random() * 10),
-            Math.floor(Math.random() * 10),
+            new Ship(i, isVertical[getRandomIntBetween(0, 1)]),
+            Math.floor(getRandomIntBetween(0, 9)),
+            Math.floor(getRandomIntBetween(0, 9)),
           );
         } catch {
           // Failed placing ship, will retry
         }
       }
+    }
+  }
+}
+
+export class CPU extends Player {
+  constructor() {
+    super();
+    this.mode = 'random';
+    this.lastHits = [];
+  }
+
+  // Helper if no ship hit
+  randomAttack(enemyBoard) {}
+
+  // Helper for inteligent attack when ship hit
+  targetAttack(enemyBoard) {}
+
+  // Main attack method
+  attack(enemyBoard) {
+    if (this.mode === 'random') {
+      return this.randomAttack(enemyBoard);
+    } else {
+      return this.targetAttack(enemyBoard);
     }
   }
 }
---
+++ src/engine/utils/utils.js
@@ -0,0 +1,3 @@
+export function getRandomIntBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
