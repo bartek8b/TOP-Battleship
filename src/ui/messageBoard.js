57840bfc3851bece63592266b7d7ab2b226b8380
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-30 00:05:42 +0100

    Add module for message board messages serving

---
+++ src/ui/messageBoard.js
@@ -0,0 +1,27 @@
+export class MessageBoard {
+  constructor() {}
+
+  welcome() {
+    return 'Battleship 1.0.0 by 8b';
+  }
+
+  placeShips(){
+    return 'Place your ships on the board'
+  }
+
+  miss(player) {
+    return `${player.name} missed!`;
+  }
+
+  accurate(player) {
+    return `${player.name} hit the opponents ship!`;
+  }
+
+  sunk(player, ship) {
+    return `${player.name} sunk the opponents ${ship.name}!`;
+  }
+
+  allSunk(player) {
+    return `${player.name} sunk the entire opponents fleet!`;
+  }
+}
