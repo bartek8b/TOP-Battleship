a58b59946999b29e21e0d6d1f7ec3f14110099f0
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-30 22:15:46 +0100

    Set updateGrid() & connect with frontend

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './modern-normalize.min.css';
 import './style.css';
 
-import { resetGrid } from './ui/renderBoard';
+import { resetGrid, updateGrid } from './ui/renderBoard';
 import { Game } from './engine/gameplay';
+import { Ship } from './engine/ship';
 
 const game = new Game();
 const player1 = game.player1;
@@ -10,3 +11,13 @@ const cpu = game.cpu;
 
 resetGrid(player1);
 resetGrid(cpu);
+player1.gameboard.placeShip(new Ship(4), 2, 0);
+cpu.gameboard.placeShip(new Ship(4), 2, 0);
+game.player1Move(2, 0);
+updateGrid(cpu);
+game.cpuMove();
+updateGrid(player1);
+game.player1Move(5, 4);
+updateGrid(cpu);
+game.cpuMove();
+updateGrid(player1);
--- src/ui/renderBoard.js
+++ src/ui/renderBoard.js
@@ -17,6 +17,30 @@ function resetGrid(player) {
   }
 }
 
-function updateGrid(player) {}
+function updateGrid(player) {
+  const arr = player.gameboard.grid;
+  console.log(arr);
+  const containerId =
+    player.name === 'Player 1' ? 'plr-container' : 'cpu-container';
+  const cells = document.querySelectorAll(
+    `#${containerId} .board-container .cell`,
+  );
+
+  cells.forEach((cell) => {
+    const x = Number(cell.dataset.x);
+    const y = Number(cell.dataset.y);
+
+    cell.className = 'cell'; // reset
+
+    if (arr[x][y] === 'miss') {
+      cell.classList.add('miss');
+    } else if (arr[x][y] === 'hit') {
+      cell.classList.add('hit');
+    } else if (arr[x][y] instanceof Ship && player.name === 'Player 1') {
+      // Show player 1's own ships on board
+      cell.classList.add('ship');
+    }
+  });
+}
 
 export { resetGrid, updateGrid };
