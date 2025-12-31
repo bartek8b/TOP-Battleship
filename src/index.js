0283a2557f5db5396358f9d11d91118368f9ecad
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-12-31 18:09:52 +0100

    Add background on hit cell

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './modern-normalize.min.css';
 import './style.css';
 
-import { test } from './ui/uiTest';
+import { Game } from './engine/gameplay';
+import { setListeners } from './ui/eventListeners';
+import { resetGrid, updateGrid } from './ui/renderBoard';
 
-test();
+const game = new Game();
+const player1 = game.player1;
+const cpu = game.cpu;
+
+resetGrid(player1);
+resetGrid(cpu);
+
+player1.randomShipsPlacement();
+cpu.randomShipsPlacement();
+
+updateGrid(player1);
+updateGrid(cpu);
+
+setListeners(game);
